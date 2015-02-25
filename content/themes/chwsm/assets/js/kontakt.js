884fa28ece439285551ff8ba9d8d3983a775d303
Konrad Głowacki <konrad@webkrak.pl>
2015-02-26 00:22:48 +0100

    Adjustments

--- content/themes/chwsm/assets/js/kontakt.js
+++ content/themes/chwsm/assets/js/kontakt.js
@@ -1,46 +1,40 @@
 $(document).ready(function() {
-    $('.googleMap img').fadeTo(3000, 0);
-    $('.googleMap iframe').fadeTo(1000, 1);
+    $('.google-maps img').fadeTo(3000, 0);
+    $('.google-maps iframe').fadeTo(1000, 1);
 
     function validateEmail($email) {
-        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-        if (!emailReg.test($email) || isEmpty($email)) {
-            return false;
-        } else {
-            return true;
-        }
+      var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+      if (!emailReg.test($email) || isEmpty($email)) {
+          return false;
+      } else {
+          return true;
+      }
     }
 
     function isEmpty(str) {
-        return (!str || 0 === str.length);
+      return (!str || 0 === str.length);
     }
 
+    $(document).on("click", "#subscribe-newsletter", function() {
+      var email = $('.newsletter-form input').val();
 
-    $(document).on("click", ".newsletter a", function() {
+      if (validateEmail(email)) {
+        $.ajax({
+          type: "POST",
+          data: "signup[email]=" + email,
+          url: "https://madmimi.com/signups/subscribe/52024",
+          contentType: "application/x-www-form-urlencoded"
+        });
 
-        var email = $('.newsletter input').val();
-
-        if (validateEmail(email)) {
-            $.ajax({
-                type: "POST",
-                data: "signup[email]=" + email,
-                url: "https://madmimi.com/signups/subscribe/52024",
-                contentType: "application/x-www-form-urlencoded"
-            });
-            $('.newsletter').html('');
-            $('.newsletter').append('<span class="light success">email <b>' + email + '</b> został dodany. Dziękujemy!</span>');
-            $('.newsletter .success').fadeTo(300, 1);
-        } else {
-
-            $('.newsletter .error').remove();
-            $('.newsletter .success').remove();
-            $('.newsletter').append('<span class="light error">niepoprawny adres email</span>');
-            $('.newsletter .error').fadeTo(300, 1);
-        }
-
-
-
-
+        $('.newsletter-form').html('');
+        $('.newsletter-form').append('<span class="light success">Email <b>' + email + '</b> został dodany. Dziękujemy!</span>');
+        $('.newsletter-form .success').fadeTo(300, 1);
+      } else {
+        $('.newsletter-form .error').remove();
+        $('.newsletter-form .success').remove();
+        $('.newsletter-form').append('<span class="error">Niepoprawny adres email</span>');
+        $('.newsletter-form .error').fadeTo(300, 1);
+      }
     });
 
 });
